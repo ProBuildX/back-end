5b2a50de50c8b65df78f44d8e6684e2002c2cce1
jufer10 <fcarrasco023@gmail.com>
2024-05-25 17:04:50 -0500

    initial commit

---
+++ src/main/java/com/construtech/buildsphere/platform/BuildspherePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.construtech.buildsphere.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildspherePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildspherePlatformApplication.class, args);
+    }
+
+}
