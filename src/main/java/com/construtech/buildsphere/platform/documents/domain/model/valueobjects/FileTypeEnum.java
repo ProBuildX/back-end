f00fbbacd136bea37cd5c0f3f8e483ad885d630d
acrispin06 <alvarocrispin0604@gmail.com>
2024-06-05 15:39:14 -0500

    feat: creating type of files for documents

--- src/main/java/com/construtech/buildsphere/platform/documents/domain/model/valueobjects/FileTypeEnum.java
+++ src/main/java/com/construtech/buildsphere/platform/documents/domain/model/valueobjects/FileTypeEnum.java
@@ -1,6 +1,5 @@
 package com.construtech.buildsphere.platform.documents.domain.model.valueobjects;
 
-// Definición de la enumeración para FileType
 public enum FileTypeEnum {
     PDF, DOCX, XLSX, TXT, PNG, JPG
 }
