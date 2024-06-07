bb3f3b34d3a5d473e2a4615fcb7259d2d5f356c0
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-07 00:56:17 -0500

    feat: document repository

--- src/main/java/com/construtech/buildsphere/platform/documents/infraestructure/persistence/jpa/repositories/DocumentRepository.java
+++ src/main/java/com/construtech/buildsphere/platform/documents/infraestructure/persistence/jpa/repositories/DocumentRepository.java
@@ -1,4 +1,14 @@
 package com.construtech.buildsphere.platform.documents.infraestructure.persistence.jpa.repositories;
 
-public interface DocumentRepository {
+import com.construtech.buildsphere.platform.documents.domain.model.aggregates.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long>{
+    Optional<Document> findByDocumentId(String documentId);
+    Optional<Document> findByDocumentIdAndProjectId(String documentId, Long projectId);
 }
