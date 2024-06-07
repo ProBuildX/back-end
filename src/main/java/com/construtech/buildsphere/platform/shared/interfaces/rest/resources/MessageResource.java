e3ddf0de3644782db420b260ae50d544f3a8cbfd
Flower PowerA <kiabailac6@gmail.com>
2024-06-07 01:19:48 -0500

    feat: document controller

--- src/main/java/com/construtech/buildsphere/platform/shared/interfaces/rest/resources/MessageResource.java
+++ src/main/java/com/construtech/buildsphere/platform/shared/interfaces/rest/resources/MessageResource.java
@@ -1,4 +1,8 @@
 package com.construtech.buildsphere.platform.shared.interfaces.rest.resources;
 
+<<<<<<< HEAD
 public record MessageResource(String message) {
+=======
+public record MessageResource(String Message) {
+>>>>>>> 1960f34 (feat: interface for message)
 }
