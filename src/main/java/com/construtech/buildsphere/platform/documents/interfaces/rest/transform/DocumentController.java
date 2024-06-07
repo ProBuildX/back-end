c9e989f25fa8239d195e6d05425d6021c5ceec7f
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-07 01:19:13 -0500

    feat: document controller

---
+++ src/main/java/com/construtech/buildsphere/platform/documents/interfaces/rest/transform/DocumentController.java
@@ -0,0 +1,27 @@
+package com.construtech.buildsphere.platform.documents.interfaces.rest.transform;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+/*
+* Document Controller
+* <p>
+* This class is responsible for handling the document requests
+* <ul>
+*       <li>POST /api/v1/projects/{id}/documents</li>
+*       <li>GET /api/v1/documents</li>
+*       <li>GET /api/v1/projects/{id}/documents</li>
+*       <li>PUT /api/v1/projects/{id}/documents/{documentId}</li>
+*       <li>DELETE /api/v1/projects/{id}/documents/{documentId}</li>
+*  </ul>
+*  </p>
+ *
+ */
+@RestController
+@RequestMapping(value = "api/v1/documents", produces = APPLICATION_JSON_VALUE)
+public class DocumentController {
+
+
+}
