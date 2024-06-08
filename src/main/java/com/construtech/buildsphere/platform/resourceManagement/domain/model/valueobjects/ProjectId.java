75406188ef40936d35bf18b9a39a0ac7aabe64a8
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-08 11:25:05 -0500

    feat: added value objects for the aggregates.

---
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/valueobjects/ProjectId.java
@@ -0,0 +1,12 @@
+package com.construtech.buildsphere.platform.resourceManagement.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ProjectId(Long projectId) {
+    public ProjectId {
+        if (projectId == null || projectId <= 0) {
+            throw new IllegalArgumentException("ProjectId cannot be null ");
+        }
+    }
+}
