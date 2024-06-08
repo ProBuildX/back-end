c0028b397b948738d8525ca06a9689cb2842698e
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-08 16:15:20 -0500

    feat: created command for creating a machine.

---
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/CreateMachineCommand.java
@@ -0,0 +1,30 @@
+package com.construtech.buildsphere.platform.resourceManagement.domain.model.commands;
+
+public record CreateMachineCommand(String machineName, String description, String receptionDate, String endDate,
+                                   double totalCost, String status, Long projectId) {
+    public CreateMachineCommand {
+        if (projectId <= 0) {
+            throw new IllegalArgumentException("Project ID must be greater than 0");
+        }
+
+        if (machineName == null || machineName.isBlank()) {
+            throw new IllegalArgumentException("Machine name cannot be null or empty");
+        }
+
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("Machine description cannot be null or empty");
+        }
+
+        if (receptionDate == null || receptionDate.isBlank()) {
+            throw new IllegalArgumentException("Reception date cannot be null or empty");
+        }
+
+        if (totalCost <= 0) {
+            throw new IllegalArgumentException("Total cost must be greater than 0");
+        }
+
+        if (status == null || status.isBlank()) {
+            throw new IllegalArgumentException("Status cannot be null or empty");
+        }
+    }
+}
