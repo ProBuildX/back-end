4b9c370313ecb1b75e681b764890a559e3b85d51
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-08 16:26:31 -0500

    feat: added the create command for Machine.java.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/aggregates/Machine.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/aggregates/Machine.java
@@ -1,5 +1,84 @@
 package com.construtech.buildsphere.platform.resourceManagement.domain.model.aggregates;
 
-public class Machine {
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.CreateMachineCommand;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.valueobjects.ProjectId;
+import com.construtech.buildsphere.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.apache.logging.log4j.util.Strings;
+
+import java.time.LocalDate;
+
+@Entity
+public class Machine extends AuditableAbstractAggregateRoot<Machine> {
+    @Embedded
+    private ProjectId projectId;
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Getter
+    private String machineName;
+
+    @Column(nullable = false)
+    @Getter
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    @Getter
+    private LocalDate receptionDate;
+
+    @Column(nullable = false)
+    @Getter
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    @Getter
+    private double totalCost;
+
+    public Machine() {
+        this.projectId = new ProjectId();
+        this.machineName = Strings.EMPTY;
+        this.description = Strings.EMPTY;
+        this.receptionDate = LocalDate.now();
+        this.endDate = null;
+        this.totalCost = 0.0;
+    }
+
+    public Machine(ProjectId projectId, String machineName, String description, LocalDate receptionDate, LocalDate endDate, double totalCost) {
+        this();
+        this.projectId = projectId;
+        this.machineName = machineName;
+        this.description = description;
+        this.receptionDate = receptionDate;
+        this.endDate = endDate;
+        this.totalCost = totalCost;
+    }
+
+    public Machine(CreateMachineCommand command) {
+        this.machineName = command.machineName();
+        this.description = command.description();
+        this.receptionDate = LocalDate.parse(command.receptionDate());
+        this.endDate = LocalDate.parse(command.endDate());
+        this.totalCost = command.totalCost();
+        this.projectId = new ProjectId(command.projectId());
+    }
+
+    public Machine updateMachine(ProjectId projectId, String machineName, String description, LocalDate receptionDate, LocalDate endDate, double totalCost) {
+        this.projectId = projectId;
+        this.machineName = machineName;
+        this.description = description;
+        this.receptionDate = receptionDate;
+        this.endDate = endDate;
+        this.totalCost = totalCost;
+        return this;
+    }
+
+    public Long getProjectId() {
+        return this.projectId.projectId();
+    }
 
 }
