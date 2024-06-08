54820a74fa3754282799be11c868ba94fc6cb687
Flower PowerA <kiabailac6@gmail.com>
2024-06-08 15:36:30 -0500

    feat: added command for create material.

---
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/CreateMaterialCommand.java
@@ -0,0 +1,34 @@
+package com.construtech.buildsphere.platform.resourceManagement.domain.model.commands;
+
+public record CreateMaterialCommand(String materialName, String materialDescription, String receptionDate,
+                                    int amount, double totalCost, String status, int projectId) {
+    public CreateMaterialCommand {
+        if (projectId <= 0) {
+            throw new IllegalArgumentException("Project ID must be greater than 0");
+        }
+
+        if (materialName == null || materialName.isBlank()) {
+            throw new IllegalArgumentException("Material name cannot be null or empty");
+        }
+
+        if (materialDescription == null || materialDescription.isBlank()) {
+            throw new IllegalArgumentException("Material description cannot be null or empty");
+        }
+
+        if (receptionDate == null || receptionDate.isBlank()) {
+            throw new IllegalArgumentException("Reception date cannot be null or empty");
+        }
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
+
+        if (totalCost <= 0) {
+            throw new IllegalArgumentException("Total cost must be greater than 0");
+        }
+
+        if (status == null || status.isBlank()) {
+            throw new IllegalArgumentException("Status cannot be null or empty");
+        }
+    }
+}
