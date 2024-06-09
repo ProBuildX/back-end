5f87d267ca5b67eb97439e8b3679f3bc2132a2d6
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-09 12:20:56 -0500

    refactor(addDocument): changed name to CreateDocumentResource

--- src/main/java/com/construtech/buildsphere/platform/documents/interfaces/rest/resources/AddDocumentResource.java
+++
@@ -1,4 +0,0 @@
-package com.construtech.buildsphere.platform.documents.interfaces.rest.resources;
-
-public record AddDocumentResource(String name, String description, String fileType, String url, Long projectId){
-}
