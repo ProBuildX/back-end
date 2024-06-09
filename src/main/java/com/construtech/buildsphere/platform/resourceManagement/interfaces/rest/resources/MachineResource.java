246d758e13c3f4b35d40dacf1f7ed15cda8b8c9d
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-09 12:49:09 -0500

    feat: added the resources for material and machine (create, update).

---
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/interfaces/rest/resources/MachineResource.java
@@ -0,0 +1,14 @@
+package com.construtech.buildsphere.platform.resourceManagement.interfaces.rest.resources;
+
+import java.time.LocalDate;
+
+public record MachineResource(
+        Long id,
+        String machineName,
+        String description,
+        LocalDate receptionDate,
+        LocalDate endDate,
+        double totalCost,
+        Long projectId
+) {
+}
