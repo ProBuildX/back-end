e5feb6ab6f285280cc36b472d96feae1298af249
Flower PowerA <kiabailac6@gmail.com>
2024-06-09 09:31:55 -0500

    fix: changed Long into ProjectId for projectId.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/infrastructure/persistence/jpa/repositories/MachineRepository.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/infrastructure/persistence/jpa/repositories/MachineRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface MachineRepository extends JpaRepository<Machine, Long> {
-    boolean existsByMachineIdAndProjectId(Long machineId, Long projectId);
-    boolean existsByMachineNameAndProjectId(String machineName, Long projectId);
-    boolean existsByMachineNameAndProjectIdNot(String machineName, Long projectId);
+public interface MachineRepository extends JpaRepository<Machine, ProjectId> {
+    boolean existsByMachineIdAndProjectId(Long machineId, ProjectId projectId);
+    boolean existsByMachineNameAndProjectId(String machineName, ProjectId projectId);
+    boolean existsByMachineNameAndProjectIdNot(String machineName, ProjectId projectId);
     Optional<Machine> findByMachineId(Long machineId);
-    Optional<Machine> findByMachineIdAndProjectId(Long machineId, Long projectId);
+    Optional<Machine> findByMachineIdAndProjectId(Long machineId, ProjectId projectId);
     List<Machine> findAllByProjectId(ProjectId projectId);
 }
