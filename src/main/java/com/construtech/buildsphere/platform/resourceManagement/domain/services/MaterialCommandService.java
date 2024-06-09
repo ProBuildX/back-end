ff57b23eca330d06f13954fa7515fd10ee7ebc91
Flower PowerA <kiabailac6@gmail.com>
2024-06-09 09:10:47 -0500

    fix: added two more handlers for update and delete respectively.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/services/MaterialCommandService.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/services/MaterialCommandService.java
@@ -2,9 +2,13 @@ package com.construtech.buildsphere.platform.resourceManagement.domain.services;
 
 import com.construtech.buildsphere.platform.resourceManagement.domain.model.aggregates.Material;
 import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.CreateMaterialCommand;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.DeleteMaterialCommand;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.UpdateMaterialCommand;
 
 import java.util.Optional;
 
 public interface MaterialCommandService {
     Optional<Material> handle(CreateMaterialCommand command);
+    Optional<Material> handle(UpdateMaterialCommand command);
+    void handle(DeleteMaterialCommand command);
 }
