8b4e36e253a69ead7d5bf608f0f7c91ebf5d6f0a
jufer10 <fcarrasco023@gmail.com>
2024-06-09 15:40:03 -0500

    fix: deleted delimitations.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/UpdateMachineCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/UpdateMachineCommand.java
@@ -2,21 +2,4 @@ package com.construtech.buildsphere.platform.resourceManagement.domain.model.com
 
 public record UpdateMachineCommand(Long id, String machineName, String description, String endDate,
                                    double totalCost) {
-    public UpdateMachineCommand {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException("Id must be greater than 0");
-        }
-
-        if (machineName == null || machineName.isBlank()) {
-            throw new IllegalArgumentException("Machine name cannot be null or empty");
-        }
-
-        if (description == null || description.isBlank()) {
-            throw new IllegalArgumentException("Machine description cannot be null or empty");
-        }
-
-        if (totalCost <= 0) {
-            throw new IllegalArgumentException("Total cost must be greater than 0");
-        }
-    }
 }
