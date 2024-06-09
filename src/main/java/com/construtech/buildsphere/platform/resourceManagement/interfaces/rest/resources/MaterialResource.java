a18bfef27fde8e99c53ccc24b43de72fbdd31ffc
jufer10 <fcarrasco023@gmail.com>
2024-06-09 12:49:09 -0500

    feat: added the resources for material and machine (create, update).

---
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/interfaces/rest/resources/MaterialResource.java
@@ -0,0 +1,15 @@
+package com.construtech.buildsphere.platform.resourceManagement.interfaces.rest.resources;
+
+import java.time.LocalDate;
+
+public record MaterialResource(
+        Long id,
+        String machineName,
+        String description,
+        LocalDate receptionDate,
+        int amount,
+        double totalCost,
+        String status,
+        Long projectId
+) {
+}
