ef472032e45fc5d7ba7be85062bded04606b7603
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-09 12:25:18 -0500

    feat: implemented document services

--- src/main/java/com/construtech/buildsphere/platform/documents/domain/services/DocumentCommandService.java
+++ src/main/java/com/construtech/buildsphere/platform/documents/domain/services/DocumentCommandService.java
@@ -1,13 +1,14 @@
 package com.construtech.buildsphere.platform.documents.domain.services;
 
-import com.construtech.buildsphere.platform.documents.domain.model.commands.addDocumentCommand;
-import com.construtech.buildsphere.platform.documents.domain.model.commands.deleteDocumentCommand;
-import com.construtech.buildsphere.platform.documents.domain.model.commands.updateDocumentCommand;
+import com.construtech.buildsphere.platform.documents.domain.model.aggregates.Document;
+import com.construtech.buildsphere.platform.documents.domain.model.commands.CreateDocumentCommand;
+import com.construtech.buildsphere.platform.documents.domain.model.commands.DeleteDocumentCommand;
+import com.construtech.buildsphere.platform.documents.domain.model.commands.UpdateDocumentCommand;
 
 import java.util.Optional;
 
 public interface DocumentCommandService {
-    Long handle(addDocumentCommand command);
-    Optional<Long> handle(updateDocumentCommand command);
-    void handle(deleteDocumentCommand command);
+    Long handle(CreateDocumentCommand command);
+    Optional<Document> handle(UpdateDocumentCommand command);
+    void handle(DeleteDocumentCommand command);
 }
--- src/main/java/com/construtech/buildsphere/platform/documents/domain/services/DocumentQueryService.java
+++ src/main/java/com/construtech/buildsphere/platform/documents/domain/services/DocumentQueryService.java
@@ -2,9 +2,13 @@ package com.construtech.buildsphere.platform.documents.domain.services;
 
 import com.construtech.buildsphere.platform.documents.domain.model.aggregates.Document;
 import com.construtech.buildsphere.platform.documents.domain.model.queries.GetAllDocumentsByProjectIdQuery;
+import com.construtech.buildsphere.platform.documents.domain.model.queries.GetDocumentByIdQuery;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DocumentQueryService {
+
+    Optional<Document> handle(GetDocumentByIdQuery query);
     List<Document> handle(GetAllDocumentsByProjectIdQuery query);
 }
