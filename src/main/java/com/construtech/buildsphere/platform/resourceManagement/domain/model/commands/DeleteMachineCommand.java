ad070dafc3ccc7cf809fd35f3e335974a1fa84ae
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-09 11:33:27 -0500

    fix: added id for material and machine to delete it.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/DeleteMachineCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/DeleteMachineCommand.java
@@ -1,11 +1,4 @@
 package com.construtech.buildsphere.platform.resourceManagement.domain.model.commands;
 
-public record DeleteMachineCommand(String machineName, String description, String receptionDate, String endDate,
-                                   double totalCost, String status, Long projectId) {
-
-    public DeleteMachineCommand {
-        if (projectId <= 0) {
-            throw new IllegalArgumentException("Project ID must be greater than 0");
-        }
-    }
+public record DeleteMachineCommand(Long machineId) {
 }
