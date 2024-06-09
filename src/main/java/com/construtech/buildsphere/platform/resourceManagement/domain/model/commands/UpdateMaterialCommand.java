4c82463e2c0eab35def1ab72e3faf0521591c06e
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-09 15:40:03 -0500

    fix: deleted delimitations.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/UpdateMaterialCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/UpdateMaterialCommand.java
@@ -2,29 +2,5 @@ package com.construtech.buildsphere.platform.resourceManagement.domain.model.com
 
 public record UpdateMaterialCommand(Long id, String materialName, String description, int amount, double totalCost,
                                     String status) {
-    public UpdateMaterialCommand {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException("Id must be greater than 0");
-        }
-
-        if (materialName == null || materialName.isBlank()) {
-            throw new IllegalArgumentException("Material name cannot be null or empty");
-        }
-
-        if (description == null || description.isBlank()) {
-            throw new IllegalArgumentException("Material description cannot be null or empty");
-        }
-
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Amount must be greater than 0");
-        }
-
-        if (totalCost <= 0) {
-            throw new IllegalArgumentException("Total cost must be greater than 0");
-        }
-
-        if (status == null || status.isBlank()) {
-            throw new IllegalArgumentException("Status cannot be null or empty");
-        }
-    }
 }
+
