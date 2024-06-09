aa53a1622feff52338ba817bfa2e828ac3852352
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-09 15:40:55 -0500

    fix: deleted delimitations.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/CreateMachineCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/CreateMachineCommand.java
@@ -1,26 +1,5 @@
 package com.construtech.buildsphere.platform.resourceManagement.domain.model.commands;
 
 public record CreateMachineCommand(String machineName, String description, String receptionDate, String endDate,
-                                   double totalCost, Long projectId) {
-    public CreateMachineCommand {
-        if (projectId <= 0) {
-            throw new IllegalArgumentException("Project ID must be greater than 0");
-        }
-
-        if (machineName == null || machineName.isBlank()) {
-            throw new IllegalArgumentException("Machine name cannot be null or empty");
-        }
-
-        if (description == null || description.isBlank()) {
-            throw new IllegalArgumentException("Machine description cannot be null or empty");
-        }
-
-        if (receptionDate == null || receptionDate.isBlank()) {
-            throw new IllegalArgumentException("Reception date cannot be null or empty");
-        }
-
-        if (totalCost <= 0) {
-            throw new IllegalArgumentException("Total cost must be greater than 0");
-        }
-    }
+                                   double totalCost, int projectId) {
 }
--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/CreateMaterialCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/CreateMaterialCommand.java
@@ -1,34 +1,5 @@
 package com.construtech.buildsphere.platform.resourceManagement.domain.model.commands;
 
 public record CreateMaterialCommand(String materialName, String description, String receptionDate,
-                                    int amount, double totalCost, String status, Long projectId) {
-    public CreateMaterialCommand {
-        if (projectId <= 0) {
-            throw new IllegalArgumentException("Project ID must be greater than 0");
-        }
-
-        if (materialName == null || materialName.isBlank()) {
-            throw new IllegalArgumentException("Material name cannot be null or empty");
-        }
-
-        if (description == null || description.isBlank()) {
-            throw new IllegalArgumentException("Material description cannot be null or empty");
-        }
-
-        if (receptionDate == null || receptionDate.isBlank()) {
-            throw new IllegalArgumentException("Reception date cannot be null or empty");
-        }
-
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Amount must be greater than 0");
-        }
-
-        if (totalCost <= 0) {
-            throw new IllegalArgumentException("Total cost must be greater than 0");
-        }
-
-        if (status == null || status.isBlank()) {
-            throw new IllegalArgumentException("Status cannot be null or empty");
-        }
-    }
+                                    int amount, double totalCost, String status, int projectId) {
 }
