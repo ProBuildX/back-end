0590e34c5679e683eea5219ce310017955cd4ea7
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-09 10:07:30 -0500

    fix: deleted projectId from Update Command.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/UpdateMaterialCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/model/commands/UpdateMaterialCommand.java
@@ -1,11 +1,8 @@
 package com.construtech.buildsphere.platform.resourceManagement.domain.model.commands;
 
 public record UpdateMaterialCommand(String materialName, String description, int amount, double totalCost,
-                                    String status, Long projectId) {
+                                    String status) {
     public UpdateMaterialCommand {
-        if (projectId <= 0) {
-            throw new IllegalArgumentException("Project ID must be greater than 0");
-        }
 
         if (materialName == null || materialName.isBlank()) {
             throw new IllegalArgumentException("Material name cannot be null or empty");
