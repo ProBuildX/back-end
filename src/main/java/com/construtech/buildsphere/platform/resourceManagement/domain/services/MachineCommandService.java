d347842a0a1d91e8f30d8f4927875bf5a23a1353
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-09 09:10:47 -0500

    fix: added two more handlers for update and delete respectively.

--- src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/services/MachineCommandService.java
+++ src/main/java/com/construtech/buildsphere/platform/resourceManagement/domain/services/MachineCommandService.java
@@ -2,9 +2,13 @@ package com.construtech.buildsphere.platform.resourceManagement.domain.services;
 
 import com.construtech.buildsphere.platform.resourceManagement.domain.model.aggregates.Machine;
 import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.CreateMachineCommand;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.DeleteMachineCommand;
+import com.construtech.buildsphere.platform.resourceManagement.domain.model.commands.UpdateMachineCommand;
 
 import java.util.Optional;
 
 public interface MachineCommandService {
     Optional<Machine> handle(CreateMachineCommand command);
+    Optional<Machine> handle(UpdateMachineCommand command);
+    void handle(DeleteMachineCommand command);
 }
