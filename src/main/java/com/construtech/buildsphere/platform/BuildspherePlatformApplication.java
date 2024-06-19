c30cc25229b65b71c7a5d998a05bee652151a8ba
Eduardo Trave <u20211a789@upc.edu.pe>
2024-06-19 15:58:50 -0500

    fix: added @EnableJpaAuditing for BuildspherePlatformApplication.java

--- src/main/java/com/construtech/buildsphere/platform/BuildspherePlatformApplication.java
+++ src/main/java/com/construtech/buildsphere/platform/BuildspherePlatformApplication.java
@@ -2,6 +2,7 @@ package com.construtech.buildsphere.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
