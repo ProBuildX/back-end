2e4c176d6356960dd2dd85b1974cb4cda852dd32
jufer10 <fcarrasco023@gmail.com>
2024-06-25 15:09:13 -0500

    feat(iam):  implemented user command service

--- src/main/java/com/construtech/buildsphere/platform/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/com/construtech/buildsphere/platform/iam/domain/model/commands/SignUpCommand.java
@@ -4,5 +4,5 @@ import com.construtech.buildsphere.platform.iam.domain.model.entities.Role;
 
 import java.util.List;
 
-public record SignUpCommand(String username, String password, List<Role> roles) {
+public record SignUpCommand(String username, String password, List<String> roles) {
 }
